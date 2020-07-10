958e57334f96211d82ad135a635badd3e5a23ec3
Caroline <c_bezzina@hotmail.com>
2020-07-11 06:16:19 +1000

    create build

---
+++ build/precache-manifest.92e78011e22b4cc054c797d9eb1fa300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f291d4f00b25493d9cee194bdf1f55c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "029dda868e20a268ddb0",
+    "url": "/static/css/main.664991ee.chunk.css"
+  },
+  {
+    "revision": "a87696d314df2f7529bd",
+    "url": "/static/js/2.e5abc3ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5abc3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "029dda868e20a268ddb0",
+    "url": "/static/js/main.0b561f4b.chunk.js"
+  },
+  {
+    "revision": "0e5cee7df415f26566b1",
+    "url": "/static/js/runtime-main.db0d703c.js"
+  }
+]);
